dbf6d4bea0dafeeed94efea1ce4a36a745406496
carexpilosa <chicoantares@web.de>
2018-03-14 23:33:29 +0100

    chesscrosstable dev

--- src/components/ChessCrossTable.js
+++ src/components/ChessCrossTable.js
@@ -11,19 +11,52 @@ class ChessCrossTable extends React.Component {
   
   render() {
     console.log(this.state.spieler);
+    const spieler = this.state.spieler;
     return (
       <div>
         <h3>Cross Table</h3>
         <ul>
         {
-          this.state.spieler.map((sp, idx) => <li key={idx}>{sp}</li>)
+          spieler.map((sp, idx) => <li key={idx}>{sp}</li>)
         }
         </ul>
         <input onChange={this.inputOnChange.bind(this)} type="text" 
+          onKeyPress={this.inputOnKeyDown.bind(this)}
           value={this.state.inputValue} />
         <a href="#" onClick={this.aOnClick.bind(this)}>add</a>
+        <p/>
+        <table>
+          <tbody>
+          <tr><td></td>
+        {
+          spieler.map((sp, idx) => {
+            return <td style={{border: '2px solid black'}} key={idx}>{sp }</td>;
+          })
+        }
+        </tr>
+        {
+          spieler.map((sp, idx) => {
+            return <tr key={idx}><td style={{border: '2px solid black'}}>{sp}</td>
+            {
+              this.state.spieler.map((sp, idx) => <td style={{border: '2px solid black'}} 
+                key={idx}>- : -</td>)
+            }
+            </tr>;
+          })
+        }
+        </tbody>
+        </table>
       </div>
     );
+  }
+
+  inputOnKeyDown(e) {
+    if(e.key === 'Enter') {
+      this.setState({
+        spieler: [ ...this.state.spieler, e.target.value ],
+        inputValue: ''
+      });
+    }
   }
 
   aOnClick(e) {
