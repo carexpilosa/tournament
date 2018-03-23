89620939670f040a364af574ee9f996caa64ce11
carexpilosa <chicoantares@web.de>
2018-03-23 09:34:25 +0100

    input for results with flexbox

--- src/components/Tournament/Pairings.js
+++ src/components/Tournament/Pairings.js
@@ -19,7 +19,34 @@ class Pairings extends React.Component {
         {
           Array.from(pairings).map(([round, val], index) => {
             const pairingsOfRound = Array.from(val).map(([whiteID, blackID], idx) => {
-              return <div key={idx}><strong>{idx + 1}</strong>: {whiteID}. {players[whiteID].name} - {blackID}. {players[blackID].name}</div>;
+              return <div key={idx} style={{
+                display: 'flex',
+                width: '520px'
+              }}>
+                <div style={{
+                  width: '370px',
+                  display: 'flex', backgroundColor: idx % 2 ? 'white' : 'gray'
+                }}>
+                  <div style={{width: '20px'}}>
+                    <strong>{idx + 1}</strong>:
+                  </div>
+                  <div style={{width: '150px'}}>
+                    {whiteID}. {players[whiteID].name}
+                  </div>
+                  <div style={{width: '90px'}}>
+                    <select name={`${round}_${whiteID}_${blackID}_white`} defaultValue=""
+                      style={{textAlignLast: 'center'}}>
+                      <option value="empty">- : -</option>
+                      <option value="0">0 : 1</option>
+                      <option value="0.5">1/2 : 1/2</option>  
+                      <option value="1">1 : 0</option>  
+                    </select>
+                  </div>
+                  <div style={{width: '150px'}}>
+                    {blackID}. {players[blackID].name} 
+                  </div>
+                </div>
+              </div>;
             });
             return (<div key={index}>
               <h3>Pairings of round {round}</h3>
