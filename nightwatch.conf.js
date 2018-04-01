ff2406620b815cee32c6145f6c7e7549b106cfd8
carexpilosa <chicoantares@web.de>
2018-04-01 23:07:01 +0200

    moved nightwatch tests to directory 'seleniumtests'

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -6,7 +6,7 @@ const chromedriver = require('chromedriver');
 // we use a nightwatch.conf.js file so we can include comments and helper functions
 module.exports = {
   'src_folders': [
-    'tests' // Where you are storing your Nightwatch e2e tests
+    'seleniumtests' // Where you are storing your Nightwatch e2e tests
   ],
   // 'output_folder': './reports', // reports (test outcome) output by nightwatch
   'selenium': {
