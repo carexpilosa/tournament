be1b210cf24f3aab2391945e06d5bfc4f478a700
carexpilosa <chicoantares@web.de>
2018-04-06 09:27:46 +0200

    edit with correct id

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -12,18 +12,18 @@ class PlayerList extends React.Component {
   render() {
     const players = this.props.players;
     let divs = [];
-    players.map((sp, idx) => {
-      divs.push(this.state.idOfPlayerToEdit === idx
+    players.map(sp => {
+      divs.push(this.state.idOfPlayerToEdit === sp.id
         ? <div>
             <input type="text" defaultValue={sp.name}
               onChange={this.updatePlayerName.bind(this)}
               onKeyPress={this.inputKeyPress} />
           </div>
         : <div style={{border: '1px solid green'}}>{sp.id} {sp.name}</div>);
-      divs.push(this.state.idOfPlayerToEdit === idx
+      divs.push(this.state.idOfPlayerToEdit === sp.id
         ? <button onClick={this.updatePlayer.bind(this)}>ok</button>
-        : <div><button onClick={e => this.edit(e, idx)}>edit</button></div>);
-      divs.push(this.state.idOfPlayerToEdit === idx
+        : <div><button onClick={e => this.edit(e, sp.id)}>edit</button></div>);
+      divs.push(this.state.idOfPlayerToEdit === sp.id
         ? <div></div>
         : <div><button>delete</button></div>);
     });
